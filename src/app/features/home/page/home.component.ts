b15625b7cf636e3bb9c3e8f60cb783130530bf75
Kerolos2000 <kero.magdy2000@gmail.com>
2026-01-31 00:52:37 +0200

    feat: 🎸 Add Categories Section in Home Page

--- src/app/features/home/page/home.component.ts
+++ src/app/features/home/page/home.component.ts
@@ -1,12 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CategoriesSectionComponent } from '../sections/categories-section/categories-section.component';
 import { HeroSectionComponent } from '../sections/hero-section/hero-section.component';
 import { SelectedArticlesSectionComponent } from '../sections/selected-articles-section/selected-articles-section.component';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, RouterModule, HeroSectionComponent, SelectedArticlesSectionComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    HeroSectionComponent,
+    SelectedArticlesSectionComponent,
+    CategoriesSectionComponent,
+  ],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {}
---
+++ src/app/features/home/sections/categories-section/categories-section.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { SectionLabelComponent } from '../../../../shared/components/section-label/section-label.component';
+
+interface Category {
+  title: string;
+  count: number;
+  icon: string;
+  delay: number;
+}
+
+@Component({
+  selector: 'app-categories-section',
+  imports: [RouterLink, SectionLabelComponent],
+  templateUrl: './categories-section.component.html',
+})
+export class CategoriesSectionComponent {
+  categories: Category[] = [
+    {
+      title: 'إضاءة',
+      count: 3,
+      icon: 'fa-sun',
+      delay: 0,
+    },
+    {
+      title: 'بورتريه',
+      count: 3,
+      icon: 'fa-user',
+      delay: 100,
+    },
+    {
+      title: 'مناظر طبيعية',
+      count: 2,
+      icon: 'fa-mountain-sun',
+      delay: 200,
+    },
+    {
+      title: 'تقنيات',
+      count: 5,
+      icon: 'fa-sliders',
+      delay: 300,
+    },
+    {
+      title: 'معدات',
+      count: 3,
+      icon: 'fa-camera',
+      delay: 400,
+    },
+  ];
+}
