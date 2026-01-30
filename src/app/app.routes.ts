d965e1607e68962b055a00a739470cf0bd643249
Kerolos2000 <kero.magdy2000@gmail.com>
2026-01-30 16:46:31 +0200

    feat: 🎸 add base of routes in app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./features/home/home.component').then((m) => m.HomeComponent),
+    title: 'عدسة - عالم التصوير',
+  },
+  {
+    path: 'blog',
+    loadComponent: () => import('./features/blog/blog.component').then((m) => m.BlogComponent),
+    title: 'المدونة - عدسة',
+  },
+  {
+    path: 'about',
+    loadComponent: () => import('./features/about/about.component').then((m) => m.AboutComponent),
+    title: 'من نحن - عدسة',
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./features/not-found/not-found.component').then((m) => m.NotFoundComponent),
+    title: 'عدسة - الصفحة غير موجودة',
+  },
+];
