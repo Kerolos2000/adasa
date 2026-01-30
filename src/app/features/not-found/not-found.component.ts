e759c8e3271e854bcfc149f84240def34309a457
Kerolos2000 <kero.magdy2000@gmail.com>
2026-01-30 18:33:22 +0200

    feat: 🎸 Add Not Found Component

--- src/app/features/not-found/not-found.component.ts
+++ src/app/features/not-found/not-found.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { BackgroundGridComponent } from '../../shared/components/background-grid/background-grid.component';
 
 @Component({
   selector: 'app-not-found',
 
-  imports: [RouterModule],
+  imports: [BackgroundGridComponent],
   templateUrl: './not-found.component.html',
 })
 export class NotFoundComponent {}
