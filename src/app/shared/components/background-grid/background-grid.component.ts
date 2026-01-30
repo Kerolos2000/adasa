3c5f5d04dcd7325207f5cccccb41908269159d6f
Kerolos2000 <kero.magdy2000@gmail.com>
2026-01-30 18:02:33 +0200

    feat: 🎸 Add Background Grid Component

---
+++ src/app/shared/components/background-grid/background-grid.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-background-grid',
+  templateUrl: './background-grid.component.html',
+})
+export class BackgroundGridComponent {}
